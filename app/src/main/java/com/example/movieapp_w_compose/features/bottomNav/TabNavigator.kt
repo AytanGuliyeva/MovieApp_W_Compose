fda8ec38528c3b7f256dcb4a9c25a2fd5867537c
AytanGuliyeva <aytnguliyeva@gmail.com>
2025-09-10 12:14:48 +0400

    refresh

--- app/src/main/java/com/example/movieapp_w_compose/features/bottomNav/TabNavigator.kt
+++ app/src/main/java/com/example/movieapp_w_compose/features/bottomNav/TabNavigator.kt
@@ -1,6 +1,5 @@
 package com.example.movieapp_w_compose.features.bottomNav
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
